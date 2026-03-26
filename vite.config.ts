a8b17f2d76df04dc14ab488b76175b92f1909f59
eJayYoung <ejay_young@163.com>
2026-03-26 14:48:02 +0800

    fix: vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import sourceIdentifierPlugin from 'vite-plugin-source-identifier'
 
 const isProd = process.env.BUILD_MODE === 'prod'
 export default defineConfig({
-  base: '/solar-terms-demo/',
+  base: '/',
   plugins: [
     react(),
     sourceIdentifierPlugin({
